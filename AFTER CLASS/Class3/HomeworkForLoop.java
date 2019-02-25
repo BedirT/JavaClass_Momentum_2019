a46443fdd4fe2be7be7644bc6caf9c4fb684d69a
BedirT <mtapkan@na.edu>
2019-02-24 19:21:25 -0600

    Hw added

--- AFTER CLASS/Class3/HomeworkForLoop.java
+++ AFTER CLASS/Class3/HomeworkForLoop.java
@@ -1,9 +1,10 @@
-// 3rd Class Homework
+import java.util.Random;
+import java.util.Scanner;
+
 
 public class HomeworkForLoop {
 
 	public static void main(String[] args) {
-		
 		/*
 		  
 		   Write a program that generates a random number and asks the user to guess what the number is. 
@@ -13,8 +14,21 @@ public class HomeworkForLoop {
 		   guesses the random number.
 		  
 		 */
+		Scanner scan = new Scanner(System.in);
+		Random rand = new Random();
+		int number = rand.nextInt(100), guesses = 0;
+		System.out.print("Can you guess the number? (1-100)");
+		for(int userGuess = 0 ; userGuess != number; guesses++) {
+			System.out.print("Guess: ");
+			userGuess = scan.nextInt();
+			if(userGuess < number) {
+				System.out.println("Up");
+			} else if(userGuess > number) {
+				System.out.println("Down");
+			}
+		}
+		System.out.println("Congratulations! That is the number!");
+		System.out.println("It took you " + guesses+1 + " guesses!");
+		scan.close();
 		
-		
-	}
-
-}
+}}
